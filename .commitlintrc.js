f1429e6474b21a0790c9a55c41e6f4a7f4b71632
wenhao.huang <huangwenhao23@163.com>
2019-08-29 19:46:14 +0800

    重构(日志提取): 更新日志提取的功能

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -6,11 +6,7 @@ module.exports = Object.assign({}, require('./lib/lint'), {
     'subject-empty': [2, 'never'],
     'type-empty': [2, 'never'],
     'scope-empty': [2, 'never'],
-    'type-enum': [
-      2,
-      'always', // 类型定义字段
-      ['新功能', 'feat', '修复', 'fix', '性能优化', 'perf', '代码样式', 'style', '文档', 'docs', '测试', 'test', '重构', 'refactor', '构建', 'build', '持续集成', 'ci', 'chore', '撤销', 'revert']
-    ],
+    'type-enum': [2, 'always', ['新功能', '修复', '优化', '文档', 'test', 'chore', 'revert', 'WIP', 'build', 'release']],
     'scope-enum': [
       2,
       'always',
