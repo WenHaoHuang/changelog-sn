aed02be51a018d23ab8456337b5297ad591f9910
wenhao.huang <huangwenhao23@163.com>
2019-10-19 13:19:34 +0800

    chore(release): 增加脚本执行打包发布流程

---
+++ release.sh
@@ -0,0 +1,40 @@
+### 
+# @Author: wenhao.huang
+ # @UserId: 18073766
+ # @Create: 2019-08-09 20:56:41
+ ###
+#!/usr/bin/env sh
+set -e
+
+if test -n "$(git status --porcelain)"; then
+  echo 'Unclean working tree. Commit or stash changes first.' >&2;
+  exit 128;
+fi
+
+if ! git fetch --quiet 2>/dev/null; then
+  echo 'There was a problem fetching your branch. Run `git fetch` to see more...' >&2;
+  exit 128;
+fi
+
+if test "0" != "$(git rev-list --count --left-only @'{u}'...HEAD)"; then
+  echo 'Remote history differ. Please pull changes.' >&2;
+  exit 128;
+fi
+
+echo 'No conflicts.' >&2;
+
+# 删除本地所有tag
+git tag -l | xargs git tag -d
+# 同步远程仓库tag
+git fetch origin --tags
+# 更新版本
+npm run v
+# 提取日志
+npm run log
+git add -A
+git commit -m "build(变更日志): CHANGELOG"
+git push
+# 推送本地所有tag到远程仓库
+git push origin --tags
+# 发布版本
+npm publish
